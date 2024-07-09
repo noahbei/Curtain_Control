942b2309c6e962db0aba55448cffd06f921d55b2
Noah Beidelman <nbeidelman9@gmail.com>
2024-07-08 17:23:06 -0700

    update tabbar names and icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,11 +24,11 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="schedule"
         options={{
-          title: 'Explore',
+          title: 'Schedule',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={focused ? 'clipboard-clock' : 'clipboard-clock-outline'} color={color} />
           ),
         }}
       />
